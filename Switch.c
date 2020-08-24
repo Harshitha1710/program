68457b40333d9d80111b002966263697e5afccd7
Harshitha1710 <70033202+Harshitha1710@users.noreply.github.com>
2020-08-24 13:27:16 +0530

    Create Switch.c
    to choose a food item

---
+++ Switch.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	printf("pick a food item : \n1. Pizza,rs 239\n2. Burger,rs 129\n3. Pasta,rs 179\n4. FrenchFires,rs 99\n5. Sandwich,rs 149");
+	int choice=0;
+	scanf("%d", &choice);
+	switch(choice)
+	{
+		case 1 :
+			printf("Your choice is Pizza,rs 239");
+			break;
+		case 2 :
+			printf("Your choice is Pizza,rs 129");
+			break;
+		case 3 :
+		    printf("Your choice is Pizza,rs 179");
+			break;	
+		case 4 :
+		    printf("Your choice is Pizza,rs 99");
+			break;
+		case 5 :
+		    printf("Your choice is Pizza,rs 149");
+			break;	
+		default : printf("Invalid choice");	
+	
+	}
+}
